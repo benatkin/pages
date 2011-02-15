3b96c6043d17adca612be0cc00b3aa0afd04fa5c
Ben Atkin <ben@benatkin.com>
2011-02-15 16:45:24 -0700

    fixes & tweaks

--- evently/wiki/edit/data.js
+++ evently/wiki/edit/data.js
@@ -1,8 +1,11 @@
 function(doc) {
+  var markdown = doc.markdown;
+  if (markdown.replace)
+    markdown = markdown.replace('</textarea>', '&lt;/textarea>');
   return {
     _id : doc._id,
     _rev : doc._rev,
     title : doc.title,
-    markdown : doc.markdown
+    markdown : markdown
   };
 };
