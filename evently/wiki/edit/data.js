838bf13353c0549cdef006d672b04aca717ea6f6
Ben Atkin <ben@benatkin.com>
2011-02-15 18:31:58 -0700

    fixed error. argh.

--- evently/wiki/edit/data.js
+++ evently/wiki/edit/data.js
@@ -1,11 +1,10 @@
 function(doc) {
-  var markdown = doc.markdown;
-  if (markdown.replace)
-    markdown = markdown.replace('</textarea>', '&lt;/textarea>');
+  var markdown = (typeof doc.markdown === 'string') ? doc.markdown : '';
+    markdown = markdown;
   return {
     _id : doc._id,
     _rev : doc._rev,
     title : doc.title,
-    markdown : markdown
+    markdown : markdown.replace('</textarea>', '&lt;/textarea>')
   };
 };
